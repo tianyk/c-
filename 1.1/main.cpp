b342801b281a630ef74e60924f7409e976949d46
tyk <yongketian@gmail.com>
2020-05-12 20:13:32 +0800

    注释

--- 1.1/main.cpp
+++ 1.1/main.cpp
@@ -12,6 +12,8 @@ int main()
 {
     //    << 输出运算符 左侧必须是一个ostream对象。输出运算符的返回值是其左侧对象，即ostream对象。a << b 返回值是 a。
     //    >> 输入运算符 和输出运算符类似，只是其左侧接受一个istream对象
+    
+//    先出输入 (std::cout << "Enter tow numbers: ") << std::endl
     std::cout << "Enter tow numbers: " << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
